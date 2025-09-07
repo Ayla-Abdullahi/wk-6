d494e14b9958b73f9adcdc0eed80c46b8ae0abd5
Ayla-Abdullahi <166896538+Ayla-Abdullahi@users.noreply.github.com>
2025-09-07 18:40:35 +0300

    wk6 Assignment

---
+++ answers.sql
@@ -0,0 +1,47 @@
+-- ### **Question 1 🧑‍💼**  
+-- **Write an SQL query** to get the **firstName**, **lastName**, **email**, and **officeCode** of all employees.  
+-- Use an **INNER JOIN** to combine the **employees** table with the **offices** table using the **officeCode** column.
+
+USE salesDB;
+
+SELECT e.firstName, 
+       e.lastName, 
+       e.email, 
+       o.officeCode
+FROM employees AS e
+INNER JOIN offices AS o 
+       ON e.officeCode = o.officeCode;
+ 
+--  ### **Question 2 🛍️**  
+-- **Write an SQL query** to get the **productName**, **productVendor**, and **productLine** from the **products** table.  
+-- Use a **LEFT JOIN** to combine the **products** table with the **productlines** table using the **productLine** column.
+---
+
+USE salesDB;
+
+SELECT p.productName, 
+       p.productVendor, 
+       pl.productLine
+FROM products AS p
+LEFT JOIN productlines AS pl 
+       ON p.productLine = pl.productLine;
+
+-- ### **Question 3 📦**  
+-- **Write an SQL query** to retrieve the **orderDate**, **shippedDate**, **status**, and **customerNumber** for the first 10 orders.  
+-- Use a **RIGHT JOIN** to combine the **customers** table with the **orders** table using the **customerNumber** column.
+
+USE salesDB;
+
+SELECT o.orderDate, 
+       o.shippedDate, 
+       o.status, 
+       o.customerNumber
+FROM customers AS c
+RIGHT JOIN orders AS o 
+       ON c.customerNumber = o.customerNumber
+LIMIT 10;
+  -- the o.orderDate, 
+--        o.shippedDate, 
+--        o.status, 
+--        o.customerNumber
+--         belongs to the orders table so we select from orders and use right join to include all orders even if there is no matching customer.
